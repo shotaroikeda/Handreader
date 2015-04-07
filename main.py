6d643b35dd2861dd1859722d78559919aa355f12
shotaroikeda <ikeda.shot@gmail.com>
2015-04-06 21:45:17 -0500

    Generated Heatmaps

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import math
 import numpy
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages 
+import os
+from copy import deepcopy as dc
 
 # Define some constants:
 image_w = 28
@@ -17,23 +19,18 @@ training_data = open('training/traininglabels')
 # final_processed is a tuple of (0, 1, 2 ,3, 4, 5, 6, 7, 8, 9)
 # manual implementation to keep it editable to an extent
 final_processed = [[], [], [], [], [], [], [], [], [], []]
+temp_processed = []
 count_nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 def reset_all():
-    global ascii_image
-    ascii_image = open('training/trainingimages')
-
-    global training_data
-    training_data = open('training/traininglabels')
-
-    global final_processed
-    final_processed = [[], [], [], [], [], [], [], [], [], []]
-
-    global count_nums
-    count_nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+    global temp_processed
+    temp_processed = dc(final_processed)
+    
     global test_images
     test_images = file('training/testimages')
+
+    global outputfile
+    outputfile = open('results/laplace_%d/results.txt' % (laplace_const), 'w')
 
     global test_data
     test_data = file('training/testlabels')
@@ -80,21 +77,6 @@ def add_data(data, result):
             else:
                 final_processed[result][i][j]+=data[i][j]
 
-    # if result == 4:
-    #     for row in final_processed[result]:
-    #         print row
-    #     plt.pcolor(numpy.array(final_processed[result]))
-    #     plt.gca().invert_yaxis()
-    #     plt.show()
-    #     raw_input()
-    
-# def process_truth():
-#     for i in range(0, 10):
-#         divide = count_nums[i]
-#         for j in range(0, image_h):
-#             for k in range(0, image_w):
-#                 final_processed[i][j][k] /= divide
-
 ##################################################################################
 # Functions for testing
 ##################################################################################
@@ -109,7 +91,8 @@ def adjust_laplace():
     for i in range(0, 10):
         for j in range(0, image_h):
             for k in range(0, image_w):
-                final_processed[i][j][k] = (float(final_processed[i][j][k] + laplace_const) / float(count_nums[i] + laplace_const*10)) 
+                global temp_processed
+                temp_processed[i][j][k] = (float(temp_processed[i][j][k] + laplace_const) / float(count_nums[i] + laplace_const*10)) 
     print "Finished adjusting for Laplace smoothing"
 
 def conclude(test_image):
@@ -120,9 +103,9 @@ def conclude(test_image):
             for k in range(0, image_w):
                 occur = 0
                 if test_image[j][k] == 0:
-                    occur = math.log(1 - final_processed[i][j][k])
+                    occur = math.log(1 - temp_processed[i][j][k])
                 elif test_image[j][k] == 1:
-                    occur = math.log(final_processed[i][j][k])
+                    occur = math.log(temp_processed[i][j][k])
                 probability += occur
         prob_map.append(probability)
     return numpy.argmax(prob_map)
@@ -132,32 +115,37 @@ def check(num):
     if not (num == answer):
         global total_wrong
         total_wrong += 1
-        # print "num: %d\n answer: %d\n" % (num, answer)
 
 ##################################################################################
 # Graphing heat maps
 ##################################################################################
+# Can only do these once
+
 def plot_num():
-    name = "heatmaps/%d_num_plots.pdf" % (laplace_const)
-    pdf = PdfPages(name)
-
-
     column = numpy.arange(0, image_h-1)
     row = numpy.arange(0, image_w-1)
-    for num_result in final_processed:
+    for num_result in temp_processed:
+        if not os.path.exists("results/laplace_%d" % (laplace_const)):
+            os.makedirs("results/laplace_%d" % (laplace_const))
+        name = "results/laplace_%d/%d_heatmap.png" % (laplace_const, temp_processed.index(num_result))
         data = numpy.array(num_result)
         plt.pcolor(data)
         plt.gca().invert_yaxis()
-        plt.axis([1, image_h-2, image_w-2, 0])
-        plt.xticks(numpy.arange(0, image_h+1)+0.5)
-        plt.yticks(numpy.arange(0, image_w+1)+0.5)
-        plt.savefig(pdf, format='pdf')
-    pdf.close()
+        plt.axis([0, image_h-1, image_w-1, 0])
+        plt.gca().axes.get_xaxis().set_visible(False)
+        plt.gca().axes.get_yaxis().set_visible(False)
+        cb = plt.colorbar()
+        cb.set_label("Values of probability")
+        plt.savefig(name)
+        plt.close()
 
 ##################################################################################
 # Training + Postprocessing
 ##################################################################################
-outputfile = open('output.txt', 'w')
+
+if not os.path.exists("results/laplace_%d" % (laplace_const)):
+    os.makedirs("results/laplace_%d" % (laplace_const))
+outputfile = open('results/laplace_%d/results.txt' % (laplace_const), 'w')
 
 def training():
     print "Training initial data with sample size %d" % (num_data)
@@ -165,10 +153,6 @@ def training():
         data = obtain_num_text(ascii_image)
         result = obtain_result()
         add_data(data, result)
-    # print "Created a heat map, now converting to probability map"
-    # converts the heat map into a probability map
-    # process_truth()
-    # print "Finished the probability map"
     
     print "--------------------"
     print "success!"
@@ -193,25 +177,16 @@ def testing():
     outputfile.write(msg)
     test_images.close()
     test_data.close()
-    
-
-def evaluation():
-    pass
-
-def print_final():
-    for i in range(0, 10):
-        print "-----\n"
-        print "printing %d" % i
-        for j in range(0, image_h):
-            print final_processed[i][j]
+    outputfile.close()
 
 if __name__ == '__main__':
-    for r in range(0, 50):
-        training()
+    training()
+    global temp_processed
+    temp_processed = dc(final_processed)
+
+    for r in range(0, 100):
         adjust_laplace()
-        # print "Generating PDFs..."
-        # plot_num()
-        # print "Finished PDF"
+        plot_num()
         testing()
         print "--------------------------\n"
         print "Reset ALL VARIABLES, INCREASE laplace by 1"
@@ -221,5 +196,4 @@ if __name__ == '__main__':
         print "Starting Next iteration..."
         print "-------------------------\n"
 
-    output.close()
 
